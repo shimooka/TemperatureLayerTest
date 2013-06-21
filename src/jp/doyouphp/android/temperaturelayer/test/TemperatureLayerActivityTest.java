0947e2471ea70b1f1e60388ed1062a0d7cd226db
Hideyuki Shimooka <shimooka@doyouphp.jp>
2013-06-21 18:09:44 +0900

    added license header

--- src/jp/doyouphp/android/temperaturelayer/test/TemperatureLayerActivityTest.java
+++ src/jp/doyouphp/android/temperaturelayer/test/TemperatureLayerActivityTest.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2013 Hideyuki SHIMOOKA <shimooka@doyouphp.jp>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package jp.doyouphp.android.temperaturelayer.test;
 
 import jp.doyouphp.android.temperaturelayer.SettingActivity;
@@ -11,107 +27,107 @@ import android.view.KeyEvent;
 import android.widget.Button;
 
 public class TemperatureLayerActivityTest
-		extends ActivityInstrumentationTestCase2<TemperatureLayerActivity> {
-	private TemperatureLayerActivity mActivity;
-	Class<?> mResource;
+        extends ActivityInstrumentationTestCase2<TemperatureLayerActivity> {
+    private TemperatureLayerActivity mActivity;
+    Class<?> mResource;
 
-	@SuppressLint("NewApi")
-	public TemperatureLayerActivityTest() {
-		super(TemperatureLayerActivity.class);
-	}
-	
-	@SuppressLint("NewApi")
-	public TemperatureLayerActivityTest(Class<TemperatureLayerActivity> activityClass) {
-		super(activityClass);
-	}
+    @SuppressLint("NewApi")
+    public TemperatureLayerActivityTest() {
+        super(TemperatureLayerActivity.class);
+    }
 
-	@Override
-	protected void setUp() throws Exception {
-		super.setUp();
+    @SuppressLint("NewApi")
+    public TemperatureLayerActivityTest(Class<TemperatureLayerActivity> activityClass) {
+        super(activityClass);
+    }
 
-		Context context = this.getInstrumentation().getTargetContext().getApplicationContext();
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        Context context = this.getInstrumentation().getTargetContext().getApplicationContext();
         try {
             mResource = context.getClassLoader().loadClass("jp.doyouphp.android.temperaturelayer.R");
         } catch (ClassNotFoundException e) {
             throw e;
         }
-	}
-	
-	@Override
-	protected void tearDown() throws Exception {
-		super.tearDown();
-	}
-	
-	public void testOnCreate() {
-		mActivity = getActivity();
-		assertTrue(mActivity.getTitle().toString().matches(
-				"^" + getStringFromR("app_name") + " ver(\\.[0-9]){3}$"));
-		
-		Button button = (Button)mActivity.findViewById(getIdFromR("StartButton"));
-		assertEquals(getStringFromR("start_service"), button.getText().toString());
-		assertTrue(button.isEnabled());
-		
-		button = (Button)mActivity.findViewById(getIdFromR("StopButton"));
-		assertEquals(getStringFromR("stop_service"), button.getText().toString());
-		assertFalse(button.isEnabled());
-	}
-	
-	public void testButtonClick() {
-		mActivity = getActivity();
-		final Button startButton = (Button)mActivity.findViewById(getIdFromR("StartButton"));
-		final Button stopButton = (Button)mActivity.findViewById(getIdFromR("StopButton"));
-		assertTrue(startButton.isEnabled());
-		assertFalse(stopButton.isEnabled());
+    }
 
-//		TouchUtils.clickView(this, startButton);
-		mActivity.runOnUiThread(new Runnable() {
-	        @Override
-	        public void run() {
-	            startButton.performClick();
-	        }
-	    });
-		getInstrumentation().waitForIdleSync();
-		assertFalse(startButton.isEnabled());
-		assertTrue(stopButton.isEnabled());
-		
-//		TouchUtils.clickView(this, stopButton);
-		mActivity.runOnUiThread(new Runnable() {
-	        @Override
-	        public void run() {
-	            stopButton.performClick();
-	        }
-	    });
-		getInstrumentation().waitForIdleSync();
-		assertTrue(startButton.isEnabled());
-		assertFalse(stopButton.isEnabled());
-	}
-	
-	public void testClickSettingMenu() {
-		ActivityMonitor monitor = getInstrumentation().addMonitor(SettingActivity.class.getName(), null, false);
-		getInstrumentation().addMonitor(monitor);
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
 
-		mActivity = getActivity();
-		getInstrumentation().sendKeyDownUpSync(KeyEvent.KEYCODE_MENU);
-		getInstrumentation().invokeMenuActionSync(mActivity, getIdFromR("action_settings"), 0);
-		
-		Activity settingActivity = getInstrumentation().waitForMonitorWithTimeout(monitor, 2000);
-		assertEquals(1, monitor.getHits());
-		if (settingActivity != null) {
-			settingActivity.finish();
-		}
-	}
+    public void testOnCreate() {
+        mActivity = getActivity();
+        assertTrue(mActivity.getTitle().toString().matches(
+                "^" + getStringFromR("app_name") + " ver(\\.[0-9]){3}$"));
+
+        Button button = (Button)mActivity.findViewById(getIdFromR("StartButton"));
+        assertEquals(getStringFromR("start_service"), button.getText().toString());
+        assertTrue(button.isEnabled());
+
+        button = (Button)mActivity.findViewById(getIdFromR("StopButton"));
+        assertEquals(getStringFromR("stop_service"), button.getText().toString());
+        assertFalse(button.isEnabled());
+    }
+
+    public void testButtonClick() {
+        mActivity = getActivity();
+        final Button startButton = (Button)mActivity.findViewById(getIdFromR("StartButton"));
+        final Button stopButton = (Button)mActivity.findViewById(getIdFromR("StopButton"));
+        assertTrue(startButton.isEnabled());
+        assertFalse(stopButton.isEnabled());
+
+//      TouchUtils.clickView(this, startButton);
+        mActivity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                startButton.performClick();
+            }
+        });
+        getInstrumentation().waitForIdleSync();
+        assertFalse(startButton.isEnabled());
+        assertTrue(stopButton.isEnabled());
+
+//      TouchUtils.clickView(this, stopButton);
+        mActivity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                stopButton.performClick();
+            }
+        });
+        getInstrumentation().waitForIdleSync();
+        assertTrue(startButton.isEnabled());
+        assertFalse(stopButton.isEnabled());
+    }
+
+    public void testClickSettingMenu() {
+        ActivityMonitor monitor = getInstrumentation().addMonitor(SettingActivity.class.getName(), null, false);
+        getInstrumentation().addMonitor(monitor);
+
+        mActivity = getActivity();
+        getInstrumentation().sendKeyDownUpSync(KeyEvent.KEYCODE_MENU);
+        getInstrumentation().invokeMenuActionSync(mActivity, getIdFromR("action_settings"), 0);
+
+        Activity settingActivity = getInstrumentation().waitForMonitorWithTimeout(monitor, 2000);
+        assertEquals(1, monitor.getHits());
+        if (settingActivity != null) {
+            settingActivity.finish();
+        }
+    }
 
     private int getIdFromR(String name) {
-    	for (Class<?> subclass: mResource.getClasses()) {
-	        try {
-	            return subclass.getField(name).getInt(null);
-	        } catch (Exception e) {
-	            // nop
-	        }
-    	}
-    	return -1;
+        for (Class<?> subclass: mResource.getClasses()) {
+            try {
+                return subclass.getField(name).getInt(null);
+            } catch (Exception e) {
+                // nop
+            }
+        }
+        return -1;
     }
-    
+
     private String getStringFromR(String name) {
         try {
             return mActivity.getText(getIdFromR(name)).toString();
