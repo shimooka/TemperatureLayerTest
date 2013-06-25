ee52da7b8064a30086c88e6641cee2314c913046
Hideyuki Shimooka <shimooka@doyouphp.jp>
2013-06-25 16:15:41 +0900

    added license header

--- src/jp/doyouphp/android/temperaturelayer/test/TemperatureLayerConfigTest.java
+++ src/jp/doyouphp/android/temperaturelayer/test/TemperatureLayerConfigTest.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2013 Hideyuki SHIMOOKA <shimooka@doyouphp.jp>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package jp.doyouphp.android.temperaturelayer.test;
 
 import jp.doyouphp.android.temperaturelayer.R;
@@ -8,70 +24,70 @@ import android.preference.PreferenceManager;
 import android.test.InstrumentationTestCase;
 
 public class TemperatureLayerConfigTest extends InstrumentationTestCase {
-	private TemperatureLayerConfig mConfig;
-	private Context mContext;
-	private SharedPreferences mPreferences;
+    private TemperatureLayerConfig mConfig;
+    private Context mContext;
+    private SharedPreferences mPreferences;
 
-	public TemperatureLayerConfigTest() {
-		super();
-	}
+    public TemperatureLayerConfigTest() {
+        super();
+    }
 
-	@Override
-	protected void setUp() throws Exception {
-		super.setUp();
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
 
-		mContext = getInstrumentation().getTargetContext()
-				.getApplicationContext();
-		mConfig = new TemperatureLayerConfig(mContext);
-		mConfig.reset();
-		mPreferences = PreferenceManager.getDefaultSharedPreferences(mContext);
-	}
+        mContext = getInstrumentation().getTargetContext()
+                .getApplicationContext();
+        mConfig = new TemperatureLayerConfig(mContext);
+        mConfig.reset();
+        mPreferences = PreferenceManager.getDefaultSharedPreferences(mContext);
+    }
 
-	public void testInitial() {
-		assertFalse(mConfig.isStartOnBoot());
-		assertEquals(
-				mContext.getResources().getString(
-						R.string.default_temperature_unit),
-				mConfig.getTemperatureUnit());
-		assertEquals(
-				mContext.getResources().getInteger(R.integer.default_text_size),
-				mConfig.getTextSize());
-		assertEquals(
-				mContext.getResources().getInteger(R.integer.default_layout),
-				mConfig.getLayout());
-		assertEquals(mContext.getResources().getColor(R.color.default_color),
-				mConfig.getColor());
-	}
+    public void testInitial() {
+        assertFalse(mConfig.isStartOnBoot());
+        assertEquals(
+                mContext.getResources().getString(
+                        R.string.default_temperature_unit),
+                mConfig.getTemperatureUnit());
+        assertEquals(
+                mContext.getResources().getInteger(R.integer.default_text_size),
+                mConfig.getTextSize());
+        assertEquals(
+                mContext.getResources().getInteger(R.integer.default_layout),
+                mConfig.getLayout());
+        assertEquals(mContext.getResources().getColor(R.color.default_color),
+                mConfig.getColor());
+    }
 
-	public void testStartOnBoot() {
-		mPreferences.edit()
-				.putBoolean(TemperatureLayerConfig.KEY_START_ON_BOOT, true)
-				.commit();
-		assertTrue(mConfig.isStartOnBoot());
-	}
+    public void testStartOnBoot() {
+        mPreferences.edit()
+                .putBoolean(TemperatureLayerConfig.KEY_START_ON_BOOT, true)
+                .commit();
+        assertTrue(mConfig.isStartOnBoot());
+    }
 
-	public void testTemperatureUnit() {
-		mPreferences.edit()
-				.putString(TemperatureLayerConfig.KEY_TEMPERATURE_UNIT, "F")
-				.commit();
-		assertEquals("F", mConfig.getTemperatureUnit());
-	}
+    public void testTemperatureUnit() {
+        mPreferences.edit()
+                .putString(TemperatureLayerConfig.KEY_TEMPERATURE_UNIT, "F")
+                .commit();
+        assertEquals("F", mConfig.getTemperatureUnit());
+    }
 
-	public void testTextSize() {
-		mPreferences.edit().putInt(TemperatureLayerConfig.KEY_TEXT_SIZE, 20)
-				.commit();
-		assertEquals(20, mConfig.getTextSize());
-	}
+    public void testTextSize() {
+        mPreferences.edit().putInt(TemperatureLayerConfig.KEY_TEXT_SIZE, 20)
+                .commit();
+        assertEquals(20, mConfig.getTextSize());
+    }
 
-	public void testLayout() {
-		mPreferences.edit().putInt(TemperatureLayerConfig.KEY_LAYOUT, 51)
-				.commit();
-		assertEquals(51, mConfig.getLayout());
-	}
+    public void testLayout() {
+        mPreferences.edit().putInt(TemperatureLayerConfig.KEY_LAYOUT, 51)
+                .commit();
+        assertEquals(51, mConfig.getLayout());
+    }
 
-	public void testColor() {
-		mPreferences.edit()
-				.putInt(TemperatureLayerConfig.KEY_COLOR, 0x80000000).commit();
-		assertEquals(0x80000000, mConfig.getColor());
-	}
+    public void testColor() {
+        mPreferences.edit()
+                .putInt(TemperatureLayerConfig.KEY_COLOR, 0x80000000).commit();
+        assertEquals(0x80000000, mConfig.getColor());
+    }
 }
